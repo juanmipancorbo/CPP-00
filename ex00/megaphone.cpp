486b4b690b838ae4e900c2cd339b9c6aad279293
Juan Miguel Pancorbo Gutiérrez <jpancorb@cbr3s4.42barcelona.com>
2025-03-10 20:17:23 +0100

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,16 +6,27 @@
 /*   By: jpancorb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 20:31:11 by jpancorb          #+#    #+#             */
-/*   Updated: 2025/03/10 16:56:48 by jpancorb         ###   ########.fr       */
+/*   Updated: 2025/03/10 20:16:12 by jpancorb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
-
+void	strtoupper(char *str)
+{
+	while(*str)
+		std::cout << static_cast<char>(std::toupper(*str++));
+}
 
 int	main(int argc, char **argv)
 {
-	std::cout <<
-	
+	if (argc > 1)
+	{
+		while(--argc)
+			strtoupper(*++argv);
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 }
