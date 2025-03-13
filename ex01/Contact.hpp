d08893d3a59f72a6ad76b901069388485e66bed5
Juan Miguel Pancorbo Gutiérrez <jpancorb@car2s5.42barcelona.com>
2025-03-13 20:08:52 +0100

    starting classes

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,15 +6,44 @@
 /*   By: jpancorb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 19:01:38 by jpancorb          #+#    #+#             */
-/*   Updated: 2025/03/11 19:43:25 by jpancorb         ###   ########.fr       */
+/*   Updated: 2025/03/13 20:07:46 by jpancorb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef		CONTACT_HPP
 # define 	CONTACT_HPP
 
-#include 	<iostream>
+#include	<string>
 
+class Contact
+{
+	private:
+		std::string _firstName;
+		std::string _lastName;
+		std::string _nickname;
+		std::string _phoneNumber;
+		std::string _darkestSecret;
+	
+	public:
+		Contact();
+		Contact(const Contact &other);
+		Contact &operator=(const Contact &other);
+		~Contact();
 
+		void		set_firstName(std::string);
+		std::string	get_firstName(void) const;
 
-#endif
+		void		set_lastName(std::string);
+		std::string	get_lastName(void) const;
+
+		void		set_nickname(std::string);
+		std::string	get_nickname(void) const;
+
+		void		set_phoneNumber(std::string);
+		std::string	get_phoneNumber(void) const;
+
+		void		set_darkestSecret(std::string);
+		std::string	get_darkestSecret(void) const;
+};
+
+#endif // CONTACT_HPP
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,10 +6,28 @@
 /*   By: jpancorb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 19:11:42 by jpancorb          #+#    #+#             */
-/*   Updated: 2025/03/11 19:43:51 by jpancorb         ###   ########.fr       */
+/*   Updated: 2025/03/13 20:07:48 by jpancorb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include 	"PhoneBook.hpp"
 #include	"Contact.hpp"
 
+int	main()
+{
+	std::cout <<	"Wellcome to Felipe PhoneBook!\n"
+					"Do you want to \"ADD\" or \"SEARCH\" a contact?"
+					"You can also \"EXIT\" if you don't know what are you doing here..."
+					"Enter one of this three commands." << std::endl;
+
+	while (command != "EXIT")
+	{
+		if (command == "ADD")
+			//add contact
+		else if (command == "SEARCH")
+			//search contact
+		else
+			continue;
+	}
+	return 0;
+}
