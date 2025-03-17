9cc7744ed180e768bb8c93845febbd41a2854cb2
Juan Miguel Pancorbo Gutiérrez <jpancorb@cbr1s5.42barcelona.com>
2025-03-17 20:04:34 +0100

    PhoneBook class done

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,9 +6,82 @@
 /*   By: jpancorb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 19:11:40 by jpancorb          #+#    #+#             */
-/*   Updated: 2025/03/11 19:43:50 by jpancorb         ###   ########.fr       */
+/*   Updated: 2025/03/17 20:02:59 by jpancorb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include 	"PhoneBook.hpp"
-#include	"Contact.hpp"
+
+// Auxiliar function
+static	std::string formatField(const std::string &str)
+{
+	if (str.length() > 10)
+		return str.substr(0, 9) + ".";
+	else
+		return str;
+}
+
+// Default Constructor
+PhoneBook::PhoneBook() : count(0)
+{}
+
+// Copy constructor
+PhoneBook::PhoneBook(const PhoneBook &other) : count(other.count)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		contacts[i] = other.contacts[i];
+	}
+}
+
+// Destructor
+PhoneBook::~PhoneBook()
+{}
+
+// Copy assignment operator
+PhoneBook &PhoneBook::operator=(const PhoneBook &other)
+{
+	if (this != &other)
+	count = other.count;
+	for (int i = 0; i < 8; i++)
+	{
+		contacts[i] = other.contacts[i];
+	}
+	return *this;
+}
+
+// Other methods
+void	PhoneBook::addContact(const Contact &newContact)
+{
+	contacts[count % 8] = newContact;
+	count++;
+}
+
+int		PhoneBook::getContactCount() const
+{
+	return (count < 8 ? count : 8);
+}
+
+const	Contact &PhoneBook::getContact(int index) const
+{
+	return contacts[index];
+}
+
+void	PhoneBook::displayContact() const
+{
+	int num 	= getContactCount();
+	std::cout	<< std::setw(10) << "index" << "|"
+				<< std::setw(10) << "first name" << "|"
+				<< std::setw(10) << "last name" << "|"
+				<< std::setw(10) << "nick name" << std::endl;
+	for (int i = 0; i < num; i++)
+	{
+		std::cout	<< std::setw(10) << i + 1 << "|";
+		std::string	firstName	= formatField(contacts[i].get_firstName());
+		std::string	lastName 	= formatField(contacts[i].get_lastName());
+		std::string	nickName	= formatField(contacts[i].get_nickname());
+		std::cout	<< std::setw(10) << firstName << "|"
+					<< std::setw(10) << lastName << "|"
+					<< std::setw(10) << nickName << std::endl;
+	}
+}
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,15 +6,40 @@
 /*   By: jpancorb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 19:05:08 by jpancorb          #+#    #+#             */
-/*   Updated: 2025/03/11 19:43:21 by jpancorb         ###   ########.fr       */
+/*   Updated: 2025/03/17 20:03:43 by jpancorb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef		PHONEBOOK_HPP
 # define	PHONEBOOK_HPP
 
-#include 	<iostream>
+# include 	"Contact.hpp"
+# include	<iomanip>
 
+class PhoneBook
+{
+	private:
+		Contact	contacts[8];
+		int		count;
+	
+	public:
+		// Default Constructor
+		PhoneBook();
 
+		// Copy constructor
+		PhoneBook(const PhoneBook &other);
 
-#endif
+		// Destructor
+		~PhoneBook();
+		
+		// Copy assignment operator
+		PhoneBook & operator = (const PhoneBook &other);
+
+		// Other methods
+		void	addContact(const Contact &newContact);
+		int		getContactCount() const;
+		const	Contact &getContact(int index) const;
+		void	displayContact() const;
+};
+
+#endif // PHONEBOOK_HPP
